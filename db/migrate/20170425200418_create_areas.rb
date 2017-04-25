ece14e2f49591cccd52962cfd1693840d117ffa0
Phil McVay <phil.mcvay90@gmail.com>
2017-04-25 21:06:44 +0100

    scaffolded Area model

---
+++ db/migrate/20170425200418_create_areas.rb
@@ -0,0 +1,12 @@
+class CreateAreas < ActiveRecord::Migration[5.0]
+  def change
+    create_table :areas do |t|
+      t.string :name
+      t.integer :capacity
+      t.string :type
+      t.string :image
+
+      t.timestamps
+    end
+  end
+end
