4380b267e5be17ef8b18a85aa7374818ebce924a
Phil McVay <phil.mcvay90@gmail.com>
2017-05-10 12:43:21 +0100

    added London timezone

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,9 +3,10 @@ class Booking < ApplicationRecord
 
   after_initialize :default_status
 
-  validates :start_time, :end_time, :no_of_people, presence: true
+  validates :start_time, :end_time, presence: true,
+                                    overlap: {:exclude_edges => ["start_time", "end_time"], :scope => "area_id"}
 
-  validates :start_time, :end_time, overlap: {:exclude_edges => ["start_time", "end_time"], :scope => "area_id"}
+  validates :no_of_people, presence: true
 
   # Sets status to "Pending" before saving the booking request
   def default_status
