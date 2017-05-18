19e2081c794bd3f32a7169b36d0e8a928dbacb6a
Phil McVay <phil.mcvay90@gmail.com>
2017-05-18 19:24:31 +0100

    changed db version

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170511180603) do
+ActiveRecord::Schema.define(version: 20170511180209) do
 
   create_table "area_resources", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "area_id"
