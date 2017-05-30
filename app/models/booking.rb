3fedd28641d53f5fcaeb4b7499ad55cb234a6e3c
Phil McVay <Phil@Phils-MacBook-Pro.local>
2017-05-30 11:57:14 +0100

    added end_time_after_start_time validation to booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -7,11 +7,20 @@ class Booking < ApplicationRecord
   validates :start_time, :end_time, presence: true,
                                     overlap: {:exclude_edges => ["start_time", "end_time"], :scope => "area_id"}
 
+  validate :end_time_after_start_time
+
   validates :no_of_people, presence: true
 
   # Sets status to "Pending" before saving the booking request
   def default_status
     self.status ||= "Pending"
+  end
+
+  # Ensures end date/time entered is greater than start date/time
+  def end_time_after_start_time
+    if start_time > end_time
+      errors.add :end_time, "must be after Start time"
+    end
   end
 
 end
